26453f78e2fa99b5b6845b4bac3f905d417ca0c4
Sam Goyal <samarthya.goyal@jostle.me>
2024-12-06 14:38:53 -0800

    request path

--- app/main.js
+++ app/main.js
@@ -5,8 +5,13 @@ console.log("Logs from your program will appear here!");
 
  //Uncomment this to pass the first stage
  const server = net.createServer((socket) => {
-    socket.on("data",()=>{
-        socket.write("HTTP/1.1 200 OK\r\n\r\n")
+    socket.on("data",(data)=>{
+        const request = data.toString().split("\r\n")
+        const requestTarget = request[0].split(" ")[1]
+        if(requestTarget ==="/"){
+            socket.write("HTTP/1.1 200 OK\r\n\r\n")}
+        else{
+            socket.write("HTTP/1.1 404 Not Found\r\n\r\n")}
     })
    socket.on("close", () => {
      socket.end();
