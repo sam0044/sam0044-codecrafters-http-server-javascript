d1f9a729d7119fd3309fb767cf502b742d5dc197
Sam Goyal <samarthya.goyal@jostle.me>
2024-12-06 17:29:31 -0800

    file

--- app/main.js
+++ app/main.js
@@ -1,7 +1,10 @@
 const net = require("net");
+const file = require("fs")
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
+const args = process.argv.slice(2)
+const directory = args[1]
 
  //Uncomment this to pass the first stage
  const server = net.createServer((socket) => {
@@ -16,6 +19,17 @@ console.log("Logs from your program will appear here!");
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${echoString.length}\r\n\r\n${echoString}`)}
         else if(requestTarget.startsWith("/user-agent")){
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`)}
+        else if(requestTarget.startsWith("/files")){
+            const filename = requestTarget.split('/files/')[1]
+            const size = file.statSync(`${directory}${filename}`).size
+            if(file.existsSync(`${directory}${filename}`)){
+                const data = file.readFileSync(`${directory}${filename}`)
+                socket.write(`HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${size}\r\n\r\n${data.toString()}`)
+            }
+            else{
+                socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
+            }
+        }
         else{
             socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
         }
