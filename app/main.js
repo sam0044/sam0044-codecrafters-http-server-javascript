ab67ecd5e006799fb237256667803b3cc4d5fca2
Sam Goyal <samarthya.goyal@jostle.me>
2024-12-10 22:31:01 -0800

    adding encoding

--- app/main.js
+++ app/main.js
@@ -5,30 +5,39 @@ const file = require("fs")
 console.log("Logs from your program will appear here!");
 const args = process.argv.slice(2)
 const directory = args[1]
-
+const ok_code = "HTTP/1.1 200 OK"
+const not_found_code = "HTTP/1.1 404 Not Found"
  //Uncomment this to pass the first stage
  const server = net.createServer((socket) => {
     socket.on("data",(data)=>{
         const request = data.toString().split("\r\n")
+        console.log(request)
         const requestTarget = request[0].split(" ")[1]
+        console.log(requestTarget)
+        const encoding = request[4].split(" ")[1]
         const userAgent = request[2].split(" ")[1]
         const method = request[0].split(" ")[0]
         if(requestTarget ==="/"){
-            socket.write("HTTP/1.1 200 OK\r\n\r\n")}
+            socket.write(`${ok_code}\r\n\r\n`)}
         else if(requestTarget.startsWith("/echo")){
             const echoString = requestTarget.slice(6)
-            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${echoString.length}\r\n\r\n${echoString}`)}
+            if (encoding === "gzip"){
+                socket.write(`${ok_code}\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: ${echoString.length}\r\n\r\n${echoString}`)
+            }
+            else{
+                socket.write(`${ok_code}\r\nContent-Type: text/plain\r\nContent-Length: ${echoString.length}\r\n\r\n${echoString}`)}
+            }
         else if(requestTarget.startsWith("/user-agent")){
-            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`)}
+            socket.write(`${ok_code}\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`)}
         else if(requestTarget.startsWith("/files") && method==='GET'){
             const filename = requestTarget.split('/files/')[1]
             if(file.existsSync(`${directory}${filename}`)){
                 const data = file.readFileSync(`${directory}${filename}`)
                 const size = file.statSync(`${directory}${filename}`).size
-                socket.write(`HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${size}\r\n\r\n${data.toString()}`)
+                socket.write(`${ok_code}\r\nContent-Type: application/octet-stream\r\nContent-Length: ${size}\r\n\r\n${data.toString()}`)
             }
             else{
-                socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
+                socket.write(`${not_found_code}\r\n\r\n`)
             }
         }
         else if(requestTarget.startsWith("/files") && method ==='POST'){
@@ -38,7 +47,7 @@ const directory = args[1]
             socket.write("HTTP/1.1 201 Created\r\n\r\n")
         }
         else{
-            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+            socket.write(`${not_found_code}\r\n\r\n`);
         }
     })
    socket.on("close", () => {
