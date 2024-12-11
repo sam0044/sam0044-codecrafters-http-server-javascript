f1591e20764228b3865dce17bc5633ed0fb183ee
Sam Goyal <samarthya.goyal@jostle.me>
2024-12-11 00:18:13 -0800

    gzip compression and cleanup

--- app/main.js
+++ app/main.js
@@ -1,15 +1,64 @@
 const net = require("net");
 const file = require("fs")
+const zlib = require('zlib')
 
+
+// Constants
+
+const HTTP_CODE = {
+    OK : "HTTP/1.1 200 OK",
+    CREATED : "HTTP/1.1 201 Created",
+    NOT_FOUND : "HTTP/1.1 404 Not Found"
+}
+const PORT = 4221;
+const HOST = "localhost";
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 const args = process.argv.slice(2)
 const directory = args[1]
-const ok_code = "HTTP/1.1 200 OK"
-const not_found_code = "HTTP/1.1 404 Not Found"
+
+
+const routeHandlers = {
+    async handleRoot(socket){
+        socket.write(`${HTTP_CODES.OK}\r\n\r\n`)}
+    ,
+
+    async handleEcho(socket,headers,requestTarget){
+        const echoString = requestTarget.slice(6)
+        const compressedData = zlib.gzipSync(echoString);
+        if (headers['accept-encoding']?.includes('gzip')){
+                socket.write(`${HTTP_CODE.OK}\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: ${compressedData.length}\r\n\r\n`)
+                socket.write(compressedData)
+            }
+        else{
+            socket.write(`${HTTP_CODE.OK}\r\nContent-Type: text/plain\r\nContent-Length: ${echoString.length}\r\n\r\n${echoString}`)}
+    },
+    async handleAgent(socket, headers){
+        const userAgent = headers['user-agent']
+        socket.write(`${HTTP_CODE.OK}\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`)
+    },
+    async handleFiles(socket,method,requestTarget,directory){
+        const filename = requestTarget.split('/files/')[1]
+        const filePath = `${directory}${filename}`
+        if (method ==="GET"){
+            if(!file.existsSync(filePath)){
+                socket.write(`${HTTP_CODE.NOT_FOUND}\r\n\r\n`)
+            }
+            const data = file.readFileSync(filePath)
+            const size = file.statSync(filePath).size
+            socket.write(`${HTTP_CODE.OK}\r\nContent-Type: application/octet-stream\r\nContent-Length: ${size}\r\n\r\n${data.toString()}`)
+        }
+        else{
+            file.writeFileSync(filePath, data)
+            socket.write(`${HTTP_CODE.CREATED}\r\n\r\n`)
+        }
+    }
+    
+}
+
  //Uncomment this to pass the first stage
  const server = net.createServer((socket) => {
-    socket.on("data",(data)=>{
+    socket.on("data", async (data)=>{
         const request = data.toString().split("\r\n")
         const requestTarget = request[0].split(" ")[1]
         const headers = {}
@@ -19,49 +68,23 @@ const not_found_code = "HTTP/1.1 404 Not Found"
             headers[key.toLowerCase()] = value
             i++
         }
-        const userAgent = headers['user-agent']
         const method = request[0].split(" ")[0]
-        if(requestTarget ==="/"){
-            socket.write(`${ok_code}\r\n\r\n`)}
-        else if(requestTarget.startsWith("/echo")){
-            const echoString = requestTarget.slice(6)
-            if (headers.hasOwnProperty('accept-encoding')){
-                const encodingList = headers['accept-encoding'].split(",").map(item => item.trim());
-                if(encodingList.includes('gzip')){
-                    socket.write(`${ok_code}\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: ${echoString.length}\r\n\r\n${echoString}`)
-                }
-                else{
-                    socket.write(`${ok_code}\r\nContent-Type: text/plain\r\nContent-Length: ${echoString.length}\r\n\r\n${echoString}`)}
-                }
-            else{
-                socket.write(`${ok_code}\r\nContent-Type: text/plain\r\nContent-Length: ${echoString.length}\r\n\r\n${echoString}`)}
-            }
-        else if(requestTarget.startsWith("/user-agent")){
-            socket.write(`${ok_code}\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`)}
-        else if(requestTarget.startsWith("/files") && method==='GET'){
-            const filename = requestTarget.split('/files/')[1]
-            if(file.existsSync(`${directory}${filename}`)){
-                const data = file.readFileSync(`${directory}${filename}`)
-                const size = file.statSync(`${directory}${filename}`).size
-                socket.write(`${ok_code}\r\nContent-Type: application/octet-stream\r\nContent-Length: ${size}\r\n\r\n${data.toString()}`)
-            }
-            else{
-                socket.write(`${not_found_code}\r\n\r\n`)
-            }
-        }
-        else if(requestTarget.startsWith("/files") && method ==='POST'){
-            const filename = requestTarget.split('/files/')[1]
-            const data = request[request.length-1]
-            file.writeFileSync(`${directory}${filename}`, data)
-            socket.write("HTTP/1.1 201 Created\r\n\r\n")
-        }
-        else{
-            socket.write(`${not_found_code}\r\n\r\n`);
-        }
+        if (requestTarget === "/") {
+            routeHandlers.handleRoot(socket);
+          } else if (requestTarget.startsWith("/echo")) {
+            routeHandlers.handleEcho(socket,headers,requestTarget);
+          } else if (requestTarget.startsWith("/user-agent")) {
+            routeHandlers.handleUserAgent(socket, headers);
+          } else if (requestTarget.startsWith("/files")) {
+            routeHandlers.handleFiles(socket, method, requestTarget, directory);
+          } else {
+            socket.write(`${HTTP_CODE.NOT_FOUND}\r\n\r\n`);
+          }
+        
     })
    socket.on("close", () => {
      socket.end();
    });
  });
 
- server.listen(4221, "localhost");
+ server.listen(PORT,HOST);
